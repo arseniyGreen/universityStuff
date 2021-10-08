a9e1274bf2087b0ab4540a6a4b53d4fcfe4f2052
Arseniy <joab.k1b3rnet1k@protonmail.com>
2021-10-08 11:51:02 +0300

    updated

--- 2kurs/Programming/hw1-2/main.cpp
+++ 2kurs/Programming/hw1-2/main.cpp
@@ -16,7 +16,7 @@ public:
         title = producer = country = "";
         seasons = popularity = rate = date = NULL;
     };
-    Series(std::string title_, std::string producer_, std::string country_, size_t seasons_, size_t popularity_, size_t rate_, size_t date_)
+    Series(std::string title_, std::string producer_, std::string country_, short seasons_, short popularity_, short rate_, short date_)
     {
         title = title_; producer = producer_; country = country_;
         seasons = seasons_; popularity = popularity_; rate = rate_; date = date_;
@@ -25,14 +25,35 @@ public:
     std::string getProducer() const { return producer; }
     std::string getCountry() const { return country; }
     short getRate() const { return rate; }
+    short getSeasons() const { return seasons; }
+    short getPopularity() const { return popularity; }
 
     friend std::ostream& operator<< (std::ostream& stream, Series& series);
+    friend bool operator <(const Series &s1, const Series &s2);
+    friend bool operator >(const Series &s1, const Series &s2);
+    friend bool operator ==(const Series &s1, const Series &s2);
 };
 
 std::ostream& operator<< (std::ostream& stream, Series& series)
 {
-    stream << "\nTitle : " << series.getTitle() << "\nProducer : " << series.getProducer() << "\nCountry : " << series.getCountry() << "\nRate : " << series.getRate() << std::endl;
+    stream << "\nTitle : " << series.getTitle() << "\nProducer : " << series.getProducer() << "\nCountry : " << series.getCountry() << "\nRate : " \
+    << series.getRate() << "\nSeasons : " << series.getSeasons() << "\nPopularity : " << series.getPopularity() << std::endl;
     return stream;
+}
+
+bool operator <(const Series &s1, const Series &s2)
+{
+    return s1.rate < s2.rate;
+}
+
+bool operator >(const Series &s1, const Series &s2)
+{
+    return s1.rate > s2.rate;
+}
+
+bool operator ==(const Series &s1, const Series &s2)
+{
+    return s1.rate == s2.rate;
 }
 
 template<class T>
@@ -95,8 +116,29 @@ void printList(std::list<T>& lst)
 int main()
 {
     std::list<Series> lst;
-    Series got;
-    push(lst, got);
+    std::list<Series> def;
+
+    Series MrR("Mister Robot", "Sam Esmail", "USA", 4, 88, 92, 24062015);
+    Series GoT("Game Of Thrones", "David Benioff", "USA", 8, 100, 92, 17042011);
+    Series TwD("The Walking Dead", "Jolly Dale", "USA", 10, 84, 94, 31102010);
+    Series FB("Family Business", "Carl Weber", "France", 2, 73, 89, 28062019);
+
+    push(lst, MrR);
+    push(lst, GoT);
+    push(lst, TwD);
+    push(lst, FB);
+
+    def.push_back(MrR);
+    def.push_back(GoT);
+    def.push_back(TwD);
+    def.push_back(FB);
+
+    std::cout << "\nUnsorted :\n";
+    printList(def);
+
+    std::cout << "\nSorted :\n";
+    printList(lst);
+
 
     return 0;
 }
