417c93c3f9f5df981b977f2946e0736bd4db3ac9
Arseniy <joab.k1b3rnet1k@protonmail.com>
2021-10-11 12:48:15 +0300

    updated

---
+++ 2kurs/Programming/hw1-3/cmake-build-debug/CMakeFiles/hw1_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw1_3.dir/main.cpp.o"
+  "hw1_3"
+  "hw1_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw1_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
