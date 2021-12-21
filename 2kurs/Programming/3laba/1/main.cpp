5847acf7ec415e44f9d2221d2a690c990bf56310
Arseniy Arkadskov <43907861+arseniyGreen@users.noreply.github.com>
2021-12-21 23:48:42 +0300

    Upd

---
+++ 2kurs/Programming/3laba/1/main.cpp
@@ -0,0 +1,80 @@
+/* Аркадсков А.Е. КТСО-02-20 */
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+
+using std::vector;
+
+void print_matrix(vector<std::vector<int> > &matrix) {
+    std::for_each(matrix.begin(), matrix.end(), [](std::vector<int> &ivec) {
+        std::for_each(ivec.begin(), ivec.end(), [](int i) {
+            std::cout << std::left << std::setw(5) << i;
+        });
+        std::cout << std::endl;
+    });
+}
+
+template <class T>
+vector<vector<T>> Minm(vector<vector<T>>& matrix)
+{
+    vector<vector<T>> minm = matrix;
+    vector<bool> v;
+
+    for(size_t i = 0; i < minm.size(); i++)
+    {
+        v.push_back(0);
+        for(size_t j = 0; j < minm.size(); j++) minm[i][j] = 0;
+    }
+    v[0] = 1;
+    bool found = false; T max = 0;
+    for(size_t i = 0; i < matrix.size(); i++){
+        for(size_t j = 0; j < matrix.size(); j++)
+            if(matrix[i][j] > max) max = matrix[i][j];
+    }
+    while(!found)
+    {
+        T min = max + 1;
+        int jn; int in; in = jn = 0;
+        for(size_t i = 0; i < v.size(); i++)
+            if(v[i] == 1)
+                for(size_t j = 0; j < v.size(); j++)
+                    if(v[j] == 0)
+                        if((min > matrix[i][j]) && (matrix[i][j] != 0))
+                        {
+                            min = matrix[i][j];
+                            jn = j; in = i;
+                        }
+        minm[in][jn] = min; minm[jn][in] = min;
+        v[jn] = 1;
+        found = true;
+
+        for(size_t i = 0; i < v.size(); i++)
+            if(v[i] == 0) found = false;
+    }
+    return minm;
+}
+
+int main() {
+    vector<vector<int> > mat =
+            {
+                    {0, 5, 3, 6, 8, 9, 7, 8, 1, 7, 0, 0, 4, 8},
+                    {5, 0, 0, 3, 6, 9, 6, 5, 0, 8, 0, 0, 5, 6},
+                    {3, 0, 0, 2, 8, 1, 3, 0, 8, 8, 5, 5, 8, 4},
+                    {6, 3, 2, 0, 4, 6, 6, 4, 6, 8, 8, 6, 9, 4},
+                    {8, 6, 8, 4, 0, 2, 8, 0, 9, 0, 8, 2, 0, 5},
+                    {9, 9, 1, 6, 2, 0, 8, 5, 5, 9, 8, 8, 9, 8},
+                    {7, 6, 3, 6, 8, 8, 0, 3, 6, 6, 8, 1, 5, 6},
+                    {8, 5, 0, 4, 0, 5, 3, 0, 7, 1, 4, 7, 8, 5},
+                    {1, 0, 8, 6, 9, 5, 6, 7, 0, 1, 2, 5, 2, 2},
+                    {7, 8, 8, 8, 0, 9, 6, 1, 1, 0, 6, 2, 4, 8},
+                    {0, 0, 5, 8, 8, 8, 8, 4, 2, 6, 0, 8, 4, 3},
+                    {0, 0, 5, 6, 2, 8, 1, 7, 5, 2, 8, 0, 5, 5},
+                    {4, 5, 8, 9, 0, 9, 5, 8, 2, 4, 4, 5, 0, 3},
+                    {8, 6, 4, 4, 5, 8, 6, 5, 2, 8, 3, 5, 3, 0},
+            };
+    vector<vector<int>> m = Minm(mat);
+    print_matrix(m);
+
+    return 0;
+}
---
+++ 2kurs/Programming/3laba/3-4/main.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+using std::vector;
+
+void print_matrix(vector<std::vector<int> > &matrix) {
+    std::for_each(matrix.begin(), matrix.end(), [](std::vector<int> &ivec) {
+        std::for_each(ivec.begin(), ivec.end(), [](int i) {
+            std::cout << std::left << std::setw(5) << i;
+        });
+        std::cout << std::endl;
+    });
+}
+
+template <class T>
+vector<vector<T>> Minm(vector<vector<T>>& matrix)
+{
+    vector<vector<T>> minm = matrix;
+    vector<bool> v;
+
+    for(size_t i = 0; i < minm.size(); i++)
+    {
+        v.push_back(0);
+        for(size_t j = 0; j < minm.size(); j++) minm[i][j] = 0;
+    }
+    v[0] = 1;
+    bool found = false; T max = 0;
+    for(size_t i = 0; i < matrix.size(); i++){
+        for(size_t j = 0; j < matrix.size(); j++)
+            if(matrix[i][j] > max) max = matrix[i][j];
+    }
+    while(!found)
+    {
+        T min = max + 1;
+        int jn; int in; in = jn = 0;
+        for(size_t i = 0; i < v.size(); i++)
+            if(v[i] == 1)
+                for(size_t j = 0; j < v.size(); j++)
+                    if(v[j] == 0)
+                        if((min > matrix[i][j]) && (matrix[i][j] != 0))
+                        {
+                            min = matrix[i][j];
+                            jn = j; in = i;
+                        }
+        minm[in][jn] = min; minm[jn][in] = min;
+        v[jn] = 1;
+        found = true;
+
+        for(size_t i = 0; i < v.size(); i++)
+            if(v[i] == 0) found = false;
+    }
+    return minm;
+}
+
+template <class T>
+vector<int> findStep(vector<vector<T>>& matrix)
+{
+    vector<int> vers; vector<int> step;
+
+    for(size_t i = 0; i < matrix.size(); i++){ vers.push_back(0); step.push_back(0); }
+
+    vers[0] = 1; bool found = false;
+    while(!found)
+    {
+        for(size_t i = 0; i < matrix.size(); i++)
+        {
+            if(vers[i] == 1)
+            {
+                for(size_t j = 0; j < matrix.size(); j++)
+                    if(matrix[i][j] != 0)
+                    {
+                        step[i] = step[i] + 1;
+                        if(vers[j] != 2) vers[j] = 1;
+                    }
+            }
+            vers[i] = 2;
+        }
+        found = true;
+        for(size_t i = 0; i < matrix.size(); i++)
+            if(vers[i] != 2) found = false;
+    }
+    return step;
+}
+
+template<class T>
+double avgStep(vector<vector<T>>& matrix)
+{
+    vector<int> a = findStep(matrix);
+    double av = 0;
+    for(size_t i = 0; i < matrix.size(); i++) av += a[i];
+    av /= matrix.size();
+}
+
+
+int main() {
+    vector<vector<int> > mat =
+            {
+                    {0, 5, 3, 6, 8, 9, 7, 8, 1, 7, 0, 0, 4, 8},
+                    {5, 0, 0, 3, 6, 9, 6, 5, 0, 8, 0, 0, 5, 6},
+                    {3, 0, 0, 2, 8, 1, 3, 0, 8, 8, 5, 5, 8, 4},
+                    {6, 3, 2, 0, 4, 6, 6, 4, 6, 8, 8, 6, 9, 4},
+                    {8, 6, 8, 4, 0, 2, 8, 0, 9, 0, 8, 2, 0, 5},
+                    {9, 9, 1, 6, 2, 0, 8, 5, 5, 9, 8, 8, 9, 8},
+                    {7, 6, 3, 6, 8, 8, 0, 3, 6, 6, 8, 1, 5, 6},
+                    {8, 5, 0, 4, 0, 5, 3, 0, 7, 1, 4, 7, 8, 5},
+                    {1, 0, 8, 6, 9, 5, 6, 7, 0, 1, 2, 5, 2, 2},
+                    {7, 8, 8, 8, 0, 9, 6, 1, 1, 0, 6, 2, 4, 8},
+                    {0, 0, 5, 8, 8, 8, 8, 4, 2, 6, 0, 8, 4, 3},
+                    {0, 0, 5, 6, 2, 8, 1, 7, 5, 2, 8, 0, 5, 5},
+                    {4, 5, 8, 9, 0, 9, 5, 8, 2, 4, 4, 5, 0, 3},
+                    {8, 6, 4, 4, 5, 8, 6, 5, 2, 8, 3, 5, 3, 0},
+            };
+
+    vector<vector<int>> m = Minm(mat);
+    vector<int> v = findStep(m);
+    for(size_t j = 0; j < mat.size(); j++) std::cout << v[j] << '\t';
+    std::cout << '\n';
+
+    return 0;
+}
